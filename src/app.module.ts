c5e8bea14f930223757acfa9153577057643f61c
Brandon <brandonaguerodeveloper@gmail.com>
2024-02-03 14:27:23 -0500

    feat: generate products module using NestJS CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 
 // Module imports
 import { CommonModule } from './common/common.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, ProductsModule],
   controllers: [],
   providers: [],
 })
