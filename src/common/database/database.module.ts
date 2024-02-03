f49d264953cf1835050e19502c07ed043ce95590
Brandon <brandonaguerodeveloper@gmail.com>
2024-02-03 06:47:39 -0500

    feat: Add configuration for databada with typeorm

--- src/common/database/database.module.ts
+++ src/common/database/database.module.ts
@@ -1,4 +1,38 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigType } from '@nestjs/config';
+import register from '../config/environments/register.environment';
+import { ENV, isProd } from '../config/environments/variables.environment';
 
-@Module({})
+@Global()
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [register.KEY],
+      useFactory: (registerService: ConfigType<typeof register>) => {
+        let DATABASE_URL: string;
+
+        if (ENV === 'production') {
+          DATABASE_URL = registerService.db.postgres.url as string;
+        } else {
+          const { user, password, host, port, name } =
+            registerService.db.postgres;
+
+          const USER = encodeURIComponent(user as string);
+          const PASSWORD = encodeURIComponent(password as string);
+
+          DATABASE_URL = `postgres://${USER}:${PASSWORD}@${host}:${port}/${name}`;
+        }
+
+        return {
+          type: 'postgres',
+          url: DATABASE_URL,
+          synchronize: !isProd,
+          autoLoadEntities: true,
+          ssl: isProd,
+        };
+      },
+    }),
+  ],
+})
 export class DatabaseModule {}
