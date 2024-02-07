88c298dfaeae3ff99ac88a3e11f29b523066a471
Brandon <brandonaguerodeveloper@gmail.com>
2024-02-07 06:55:52 -0500

    feat(filter): Configure all-exceptions.filter.ts to handle HttpException and QueryFailedError

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import {
 import { AppModule } from './app.module';
 import { PORT } from '@env/variables.environment';
 import { SwaggerModule } from './common/swagger/swagger.module';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -22,6 +23,7 @@ async function bootstrap() {
       },
     }),
   );
+  app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   SwaggerModule.configure(app);
   await app.listen(PORT, '0.0.0.0');
